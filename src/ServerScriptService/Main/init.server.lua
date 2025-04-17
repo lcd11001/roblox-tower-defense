c9f711f3544f0aef4d8b78ef526b4cedf8c2c93a
LCD-PC <lcd11001@gmail.com>
2025-04-17 19:25:08 +0700

    + testing physic group

--- src/ServerScriptService/Main/init.server.lua
+++ src/ServerScriptService/Main/init.server.lua
@@ -3,6 +3,25 @@
 
 local Mob = require(script.Mob)
 local Map = workspace:WaitForChild("Grassland Map")
+local Mobs = Map:WaitForChild("Mobs")
+local MobWaveTime = 3
+local MobWaveTotal = 5
+local MobNames = { "Zombie", "Noob", "Mech", "Dracula" }
 
-Mob.SpawnMultiple("Zombie", Map, 3, {"ZombieMovement"})
+-- spawn waves of zombies
+for wave = 1, MobWaveTotal do
+	local mobName = wave ~= MobWaveTotal and MobNames[math.random(1, #MobNames - 1)] or MobNames[#MobNames]
+    -- local mobName = MobNames[4]
+	print("Spawning wave " .. wave .. " : " .. mobName)
+	-- print("Mob name: " .. mobName)
+	Mob.SpawnMultiple(mobName, Map, 3 * wave, { "ZombieMovement" })
+    Mob.Spawn("Mech", Map, { "ZombieMovement" })
+    -- Mob.Spawn(mobName, Map, { "ZombieMovement" })
 
+	repeat
+		task.wait(1)
+	-- print("Waiting for zombies have been destroyed")
+	until #Mobs:GetChildren() == 0
+	print("Wave " .. wave .. " cleared")
+	task.wait(MobWaveTime)
+end
