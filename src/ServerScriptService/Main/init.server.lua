9a264d842d34804e5fb7620969cb27554a15940c
LCD-PC <lcd11001@gmail.com>
2025-04-26 08:24:35 +0700

    - should store model in Roblox

--- src/ServerScriptService/Main/init.server.lua
+++ src/ServerScriptService/Main/init.server.lua
@@ -6,16 +6,16 @@ local Map = workspace:WaitForChild("Grassland Map")
 local Mobs = Map:WaitForChild("Mobs")
 local MobWaveTime = 3
 local MobWaveTotal = 5
-local MobNames = { "Zombie", "Noob", "Mech", "Dracula" }
+local MobNames = { "Zombie", "Noob", "MechCannon", "Dracula" }
 
 -- spawn waves of zombies
 for wave = 1, MobWaveTotal do
-	-- local mobName = wave ~= MobWaveTotal and MobNames[math.random(1, #MobNames - 1)] or MobNames[#MobNames]
-    local mobName = MobNames[4]
+	local mobName = wave ~= MobWaveTotal and MobNames[math.random(1, #MobNames - 1)] or MobNames[#MobNames]
+	-- local mobName = MobNames[3]
 	print("Spawning wave " .. wave .. " : " .. mobName)
 	-- print("Mob name: " .. mobName)
-	-- Mob.SpawnMultiple(mobName, Map, 3 * wave, { "ZombieMovement" })
-    Mob.Spawn(mobName, Map, { "ZombieMovement", "ZombieAnimation" })
+	Mob.SpawnMultiple(mobName, Map, 3 * wave, { "ZombieMovement", "ZombieAnimation" })
+	-- Mob.Spawn(mobName, Map, { "ZombieMovement", "ZombieAnimation" })
 
 	repeat
 		task.wait(1)
